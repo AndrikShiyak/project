8bed75534f3bc5cb348414ff99c9da2f4e620754
Andrik <andrikshiyak@gmail.com>
2020-12-19 02:38:43 +0200

    script.js upd

--- js/script.js
+++ js/script.js
@@ -6,10 +6,19 @@
 // const answer = +prompt("Вам есть 18?", "18");
 // console.log(typeof(answer));
 
-const answers = [];
+// const answers = [];
 
 // answers[0] = prompt("Как ваше имя?", "");
 // answers[1] = prompt("Как ваше фамилия?", "");
 // answers[2] = prompt("Сколько вам лет?", "");
 
-console.log(typeof(answers));
+// console.log(typeof(answers));
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+
+    console.log(i);
+}
